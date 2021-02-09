3940e6d1b24ef36e8b6cdd789002f94a2637d0b7
Jose Quesada <jquesada2016@fau.edu>
2021-02-09 12:06:59 -0500

    added tailwind colors

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -1,70 +1,26 @@
 declare module "tailwindcss/colors" {
   export const black: string;
   export const white: string;
-  export const rose: {
-    [index = number] = string;
-  };
-  export const pink: {
-    [index = number] = string;
-  };
-  export const fuchsia: {
-    [index = number] = string;
-  };
-  export const purple: {
-    [index = number] = string;
-  };
-  export const violet: {
-    [index = number] = string;
-  };
-  export const indigo: {
-    [index = number] = string;
-  };
-  export const blue: {
-    [index = number] = string;
-  };
-  export const lightBlue: {
-    [index = number] = string;
-  };
-  export const cyan: {
-    [index = number] = string;
-  };
-  export const teal: {
-    [index = number] = string;
-  };
-  export const emerald: {
-    [index = number] = string;
-  };
-  export const green: {
-    [index = number] = string;
-  };
-  export const lime: {
-    [index = number] = string;
-  };
-  export const yellow: {
-    [index = number] = string;
-  };
-  export const amber: {
-    [index = number] = string;
-  };
-  export const orange: {
-    [index = number] = string;
-  };
-  export const red: {
-    [index = number] = string;
-  };
-  export const warmGray: {
-    [index = number] = string;
-  };
-  export const trueGray: {
-    [index = number] = string;
-  };
-  export const gray: {
-    [index = number] = string;
-  };
-  export const coolGray: {
-    [index = number] = string;
-  };
-  export const blueGray: {
-    [index = number] = string;
-  };
+  export const rose: any;
+  export const pink: any;
+  export const fuchsia: any;
+  export const purple: any;
+  export const violet: any;
+  export const indigo: any;
+  export const blue: any;
+  export const lightBlue: any;
+  export const cyan: any;
+  export const teal: any;
+  export const emerald: any;
+  export const green: any;
+  export const lime: any;
+  export const yellow: any;
+  export const amber: any;
+  export const orange: any;
+  export const red: any;
+  export const warmGray: any;
+  export const trueGray: any;
+  export const gray: any;
+  export const coolGray: any;
+  export const blueGray: any;
 }
